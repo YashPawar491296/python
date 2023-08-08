5fbeb177dadf1bf6ce9702133ed5898e65947783
YashPawar491296 <141736830+YashPawar491296@users.noreply.github.com>
2023-08-08 19:33:30 +0530

    Create greater.py
    Program to find greater number from given three numbers

---
+++ greater.py
@@ -0,0 +1,13 @@
+print("\n\n\t\t Find the greatest number of the three number\n\n")
+
+
+a=int(input("Enter the number of A = "))
+b=int(input("Enter the number of B = "))
+c=int(input("Enter the number of C = "))
+
+if a>b and a>c :
+  print("A is greater")
+elif b>a and b>c :
+  print("B is greater")
+else :
+  print("C is greater")
