92c267a5034fecfc6bf8b4ac45b6923d7b748fce
YashPawar491296 <141736830+YashPawar491296@users.noreply.github.com>
2023-08-08 19:39:19 +0530

    Create swapT.py
    Program to swap the values using temporary variable

---
+++ swapT.py
@@ -0,0 +1,7 @@
+ a=int(input("Enter value of A = "))
+ b=int(input("enter value of b = "))
+ c=a
+ a=b
+ b=c
+ print("After swapping the value of A = ",a)
+ print("After swapping the value of B = ",b)
