24f4aced37b0738db6def0a998e32510313df9e7
YashPawar491296 <141736830+YashPawar491296@users.noreply.github.com>
2023-08-08 19:36:43 +0530

    Create swap1.py
    Program to swap two numbers without using another temporary variable

---
+++ swap1.py
@@ -0,0 +1,8 @@
+ a=int(input("Enter value of A = "))
+ b=int(input("enter value of b = "))
+ a=a+b
+ b=b-a
+ a=a+b
+ b=a+b
+ print("After swapping the value of A = ",a)
+ print("After swapping the value of B = ",b)
