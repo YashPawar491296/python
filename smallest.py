df10926a085d44dcf11243257c58727df0b5916a
YashPawar491296 <141736830+YashPawar491296@users.noreply.github.com>
2023-08-13 15:03:16 +0530

    Create smallest.py
    Program to find Smallest number among three.

---
+++ smallest.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the number of A = "))
+b=int(input("Enter the number of B = "))
+c=int(input("Enter the number of C = "))
+if a<b and a<c :
+  print("A is smaller")
+elif b<a and b<c :
+  print("B is smaller")
+else :
+  print("C is smaller")
