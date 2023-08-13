e397f7820916f01d615b45943fe8a8c3f8e89a95
YashPawar491296 <141736830+YashPawar491296@users.noreply.github.com>
2023-08-13 10:58:11 +0530

    Update greater.py
    Program  to find greatest among three integers.

--- greater.py
+++ greater.py
@@ -1,10 +1,7 @@
 print("\n\n\t\t Find the greatest number of the three number\n\n")
-
-
 a=int(input("Enter the number of A = "))
 b=int(input("Enter the number of B = "))
 c=int(input("Enter the number of C = "))
-
 if a>b and a>c :
   print("A is greater")
 elif b>a and b>c :
